b3cf2ffd9c6dd87b7678bb8f1fd98daa67a69675
Xavi <xavi.rmz@gmail.com>
2013-07-05 14:33:56 -0700

    Refactored code and added close_view and close_window options.

--- quit-guard.py
+++ quit-guard.py
@@ -3,11 +3,31 @@ from sublime import Window
 
 from time import time;
 
+QUIT = "Quit Now!"
+CLOSE_VIEW = "Close View"
+CLOSE_WINDOW = "Close Window"
+CANCEL = "Cancel"
+
+options = [
+	QUIT,
+	CLOSE_VIEW,
+	CLOSE_WINDOW,
+	CANCEL
+]
+
 class QuitGuardCommand(sublime_plugin.TextCommand):
 	def on_done(self, idx):
-		if idx == 1:
+		option = options[idx]
+
+		if option == CANCEL and idx < 0:
+			pass
+		elif option == QUIT:
 			self.view.window().run_command("exit")
+		elif option == CLOSE_WINDOW:
+			self.view.window().run_command("close_window")
+		elif option == CLOSE_VIEW:
+			self.view.window().run_command("close")
 
 	def run(self, edit):
 		window = self.view.window()
-		window.show_quick_panel(["Cancel", "Quit Now!"], self.on_done)
+		window.show_quick_panel(options, self.on_done)
