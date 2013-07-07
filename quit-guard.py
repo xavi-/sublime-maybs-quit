5cf606d7e486c2a27906c07a1458fa8afff8c3b9
Mandx <gmandx@gmail.com>
2013-07-06 20:44:12 -0400

    Convert tabs to spaces

--- quit-guard.py
+++ quit-guard.py
@@ -2,27 +2,27 @@ import sublime, sublime_plugin
 from sublime import Window
 
 options = [
-	"Quit Now!",
-	"Close View",
-	"Close Window",
-	"Save as...",
-	"Cancel"
+    "Quit Now!",
+    "Close View",
+    "Close Window",
+    "Save as...",
+    "Cancel"
 ]
 
 commands = {
-	"Quit Now!": "exit",
-	"Close View": "close",
-	"Close Window": "close_window",
-	"Save as...": "prompt_save_as"
+    "Quit Now!": "exit",
+    "Close View": "close",
+    "Close Window": "close_window",
+    "Save as...": "prompt_save_as"
 }
 
 class QuitGuardCommand(sublime_plugin.TextCommand):
-	def on_done(self, idx):
-		option = options[idx]
+    def on_done(self, idx):
+        option = options[idx]
 
-		if option in commands:
-			self.view.window().run_command(commands[option])
+        if option in commands:
+            self.view.window().run_command(commands[option])
 
-	def run(self, edit):
-		window = self.view.window()
-		window.show_quick_panel(options, self.on_done)
+    def run(self, edit):
+        window = self.view.window()
+        window.show_quick_panel(options, self.on_done)
