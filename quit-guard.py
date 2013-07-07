4b7f7671e359293ecb5ceed00d01f4a3cdf5a97f
Xavi <xavi.rmz@gmail.com>
2013-07-06 19:59:16 -0700

    Spaces to tabs.

--- quit-guard.py
+++ quit-guard.py
@@ -3,21 +3,21 @@ import sublime_plugin
 
 class QuitGuardCommand(sublime_plugin.TextCommand):
 
-    OPTIONS = (
-        ('Quit Now!', 'exit', ),
-        ('Close View', 'close', ),
-        ('Close Window', 'close_window', ),
-        ('Save as...', 'prompt_save_as', ),
-        ('Cancel', None, ),
-    )
+	OPTIONS = (
+		('Quit Now!', 'exit', ),
+		('Close View', 'close', ),
+		('Close Window', 'close_window', ),
+		('Save as...', 'prompt_save_as', ),
+		('Cancel', None, ),
+	)
 
-    OPTIONS_NAMES = tuple((opt[0] for opt in OPTIONS))
+	OPTIONS_NAMES = tuple((opt[0] for opt in OPTIONS))
 
-    def run(self, edit):
-        self.view.window().show_quick_panel(self.OPTIONS_NAMES, self.on_done)
+	def run(self, edit):
+		self.view.window().show_quick_panel(self.OPTIONS_NAMES, self.on_done)
 
-    def on_done(self, idx):
-        option, command = self.OPTIONS[idx]
+	def on_done(self, idx):
+		option, command = self.OPTIONS[idx]
 
-        if command:
-            self.view.window().run_command(command)
+		if command:
+			self.view.window().run_command(command)
